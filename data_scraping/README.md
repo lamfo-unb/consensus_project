f9c4ab988db0b3c487209a3ac95e2777aeac1450
MCLF13 <marciusclima@gmail.com>
2020-05-03 19:27:28 -0300

    Generalização do local do captcha
    Pegar o local do captcha em páginas com tamanhos maiores

--- data_scraping/README.md
+++ data_scraping/README.md
@@ -4,10 +4,10 @@
 ### Download and install chrome driver
 1. Step 1: https://www.swtestacademy.com/install-chrome-driver-on-mac/
 2. Step 2: https://chromedriver.storage.googleapis.com/index.html?path=83.0.4103.14/
-### The exe must be located at /usr/loca/bin
+### The chromedriver exe must be located at /usr/loca/bin
 3. *$ mv chromedriver /usr/local/bin* 
 
-** Obs:** Make sure that the installed crhome has the same version as the downloaded driver.
+** Obs:** Make sure that the installed chrome has the same version as the downloaded driver.
 
 # Libraries
 1. pip install selenium
--- data_scraping/test_selenium.py
+++ data_scraping/test_selenium.py
@@ -11,68 +11,78 @@ from PIL import Image
 from io import BytesIO
 from data_scraping import deathbycaptcha
 import time
+import os
 
 #Create the tempfile
 tmp_path = tempfile.mkdtemp()
 options = webdriver.ChromeOptions()
 options.add_argument("download.default_directory="+tmp_path)
-prefs = {'download.default_directory' : tmp_path};
+prefs = {'download.default_directory': tmp_path};
 options.add_experimental_option('prefs', prefs)
 
 #initializite the driver
-driver = webdriver.Chrome(options=options)
-delay = 3 # seconds
+driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=options)
+#TODO Loop in case of empty zip_file path (wrong captcha)
+# f_download = False
+# while not f_download:
+delay = 3  # seconds
 driver.get("http://fundamentus.com.br/balancos.php?papel=PETR4")
 WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'containerMenu')))
-#Screen shot
+# Screen shot
 element = driver.find_element_by_class_name("captcha")
-#Get location
+# Get location
 location = element.location
-#Get size
+# Get size
 size = element.size
-#Do the screen shot
+# Do the screen shot
 png = driver.get_screenshot_as_png()
-#Cut the image
+# Cut the image
 im = Image.open(BytesIO(png))
-#TODO Get the location automatically
-#left = location['x']
-#top = location['y']+300
-#right = location['x'] + size['width']
-#bottom = location['y'] + size['height']+300
-#im = im.crop((left, top, right, bottom)) # defines crop points
-im = im.crop((230, 690, 630, 800)) # defines crop points
+# Get the location automatically
+left = element.location_once_scrolled_into_view['x']
+top = element.location_once_scrolled_into_view['y']  # +300
+right = element.location_once_scrolled_into_view['x'] + size['width']
+bottom = element.location_once_scrolled_into_view['y'] + size['height']  # +300
+im = im.crop((left, top, right, bottom))  # defines crop points
+# im = im.crop((230, 690, 630, 800))  defines crop points
 im.save('captcha.png')
 
-#Send the captcha to the DeathByCaptcha
+# Send the captcha to the DeathByCaptcha
 username = "pedrobsb"
-password= "*********"
+password = "*********"
 
-#Send the captcha
+# Send the captcha
 client = deathbycaptcha.SocketClient(username, password)
 captcha = client.decode("captcha.png", 15)
 
-#Get the solution
+# Get the solution
 solution = captcha["text"]
 
-#Find the text box
+# Find the text box
 web_elem = driver.find_element("name", "codigo_captcha")
 
-#Send the captcha results
+# Send the captcha results
 web_elem.send_keys(solution)
 
-#Request the data_scraping
+# Request the data_scraping
 button = driver.find_element("name", "submit")
 
-#Click the button
+# Click the button
 button.click()
 
-#Sleep and wait for the download
+# Sleep and wait for the download
 time.sleep(2)
 driver.close()
 driver.quit()
 
-#Extract the zip file
-zip_file = glob.glob(tmp_path+"/"+"*.zip")
+# Extract the zip file
+zip_file = glob.glob(tmp_path + "/" + "*.zip")
+#TODO Loop in case of empty zip_file path (wrong captcha)
+#    if zip_file:
+#        print("File exists!")
+#        f_download = True
+#    else:
+#        print("No file in path, trying again...")
 
 #Sleep and wait for zip extract
 time.sleep(2)
