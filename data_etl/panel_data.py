8cf811b85ef39bff520205e279b15ebf525590ac
Pedro Albuquerque <pedro.melo.albuquerque@gmail.com>
2020-05-05 22:54:27 -0400

    longest common substring similarity

--- data_etl/panel_data.py
+++ data_etl/panel_data.py
@@ -10,8 +10,8 @@ with open(r'data/all_data.pkl', 'rb') as handle:
 columns_a = list(symbols.values())[0].columns.tolist()
 columns_b = list(symbols.values())[1].columns.tolist()
 
-#Create the levenshtein distance matrix
-def create_levenshtein_matrix(columns_a,columns_b):
+#Create the longest common substring similarity distance matrix
+def create_lcsstr_matrix(columns_a,columns_b):
     #Step 1: Remove accents and makes everything upper case
     columns_a = [x.upper() for x in columns_a]
     columns_a = [unidecode.unidecode(x) for x in columns_a]
@@ -23,22 +23,22 @@ def create_levenshtein_matrix(columns_a,columns_b):
     dist_mat = np.zeros((len(columns_a),len(columns_b)))
     for row in range(len(columns_a)):
         for col in range(len(columns_b)):
-            dist_mat[row,col] = textdistance.levenshtein.distance(columns_a[row],columns_b[col])
+            dist_mat[row,col] = textdistance.lcsstr.distance(columns_a[row],columns_b[col])
 
     return dist_mat
 
-dist_mat = create_levenshtein_matrix(columns_a,columns_b)
+dist_mat = create_lcsstr_matrix(columns_a,columns_b)
 
 result = np.argmin(dist_mat, axis =1)
 
-
-txt1 = columns_a[2]
+ipos = 7
+txt1 = columns_a[ipos]
 print(txt1)
-txt2 = columns_b[50]
+txt2 = columns_b[result[ipos]]
 print(txt2)
-res1 = textdistance.levenshtein.distance(columns_a[0],columns_b[1])
+res1 = textdistance.lcsstr.distance(columns_a[ipos],columns_b[result[ipos]])
 print(res1)
-res2 = dist_mat[2,50]
+res2 = dist_mat[ipos,result[ipos]]
 print(res2)
 
 
