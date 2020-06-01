5bdbda70980e28131d9899e8b2933f2f899d027f
Pedro Albuquerque <pedro.melo.albuquerque@gmail.com>
2020-06-01 15:11:42 -0400

    MIDAS

---
+++ midas/midaspy.py
@@ -0,0 +1,5 @@
+#git clone https://github.com/mikemull/midaspy
+#cd midaspy/ > python setup.py build
+#cd midaspy/ > python setup.py install
+
+#Example from: https://github.com/mikemull/midaspy/blob/master/examples/MIDASExamples.ipynb
